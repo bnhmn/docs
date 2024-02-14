9ca80dccf7b7a8bdfa09cf14760cef77f5d1526c
Benjamin Heimann <benjamin.heimann@outlook.de>
2024-02-14 17:53:56 +0100

    Move selenium script

--- selenium/simple-script/selenium_test.py
+++ selenium/simple-script/selenium_test.py
@@ -1,10 +1,9 @@
-import os
+from selenium.webdriver.common.by import By
 from time import sleep
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
-# Selenium script that automatically presses the 'Approve' button on GitHub Actions deployments
+# Selenium script that automatically presses the 'Approve' button on all outstanding GitHub Actions deployments
 
 options = webdriver.FirefoxOptions()
 options.add_argument("-profile")
@@ -16,7 +15,7 @@ driver.implicitly_wait(0.25)
 driver.get("https://github.com/ben5311/ranger2/actions")
 
 while not driver.title.startswith("Actions"):
-    print("Please login!") # TODO: add automatic login here
+    print("Please login!")  # TODO: add automatic login here
     sleep(3)
 
 action_runs = driver.find_elements(by=By.CSS_SELECTOR, value='[id^="check_suite_"]')
@@ -25,20 +24,24 @@ links = [run.find_element(by=By.TAG_NAME, value="a").get_attribute("href") for r
 
 for link in links:
     driver.get(link)
+
     buttons = driver.find_elements(by=By.CSS_SELECTOR, value="button[id^=dialog-show]")
     buttons = [button for button in buttons if "Review deployments" in button.text]
     button = buttons[0]
     button.click()
+
     checkboxes = driver.find_elements(by=By.CSS_SELECTOR,
                                       value='input[type="checkbox"][name="gate_request[]"]')
     for checkbox in checkboxes:
         if checkbox.size["height"] > 0:
             checkbox.click()
+
     submit_buttons = driver.find_elements(By.CSS_SELECTOR,
                                           'button[type="submit"][value="approved"]')
     for button in submit_buttons:
         if button.size["height"] > 0:
             button.click()
-    sleep(1)
+
+    sleep(1)  # rate limiting
 
 # driver.quit() # Don't quit so the Browser window stays open when finished
