04de696b2d5947868e7afd9b5a81ab1c516fa7e5
Benjamin Heimann <benjamin.heimann@outlook.de>
2025-03-30 14:14:15 +0200

    Add ktor-todo-list-api

---
+++ kotlin/ktor-todo-list-api/src/main/kotlin/Application.kt
@@ -0,0 +1,13 @@
+package kotlin.todolist
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureMonitoring()
+    configureSerialization()
+    configureRouting()
+}
